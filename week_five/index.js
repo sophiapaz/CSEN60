45631338340e5d66716573113241eb916e993933
sophiapaz <sophiacp900@gmail.com>
2024-10-21 11:32:00 -0700

    wrote some javascript

---
+++ week_five/index.js
@@ -0,0 +1,27 @@
+//console.log("Hello World")
+
+//alert('Hello, World!');
+
+window.document.write('<p>Welcome to CSEN 60!</p>');
+
+//document.append('<p>this should come after the h1</p>');
+
+/*
+  Function to calculate the sum of two numbers.
+  Inputs: a, b - numbers to be added.
+  Output: returns the sum of a and b.
+*/
+// TODO: fix this function
+function add(a, b) {
+    return a + b;
+}
+
+/**
+ * Adds two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @return {number} The sum of a and b.
+ */
+function add(a, b) {
+    return a + b;
+}
