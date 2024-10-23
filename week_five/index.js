5a1b6d39ab089cc6209047ca94f7ff23205938ab
sophiapaz <sophiacp900@gmail.com>
2024-10-23 11:34:50 -0700

    learned types and more javascript

--- week_five/index.js
+++ week_five/index.js
@@ -2,7 +2,7 @@
 
 //alert('Hello, World!');
 
-window.document.write('<p>Welcome to CSEN 60!</p>');
+//window.document.write('<p>Welcome to CSEN 60!</p>'); (window is only runtime in browser, not locally)
 
 //document.append('<p>this should come after the h1</p>');
 
@@ -25,3 +25,39 @@ function add(a, b) {
 function add(a, b) {
     return a + b;
 }
+
+let x = 100;
+/*
+let x => declaration;
+x = 2 => initialization
+*/
+if (true) {
+    x = 200; // Same variable!
+}
+console.log(x); // Outputs: 200
+
+let y;
+y = null;
+let z = 0;
+console.log(y, z);
+
+let greeting = "Hello, world!";
+let response = `The time is ${new Date()}`;
+console.log(response);
+
+const age = 17;
+  if(age > 18) {
+    console.log("you are of age");
+  } else {
+    console.log(`you aren't of age. Please wait ${18 - age} years`);
+  }
+
+  let person = {
+    name: "John",
+    age: 30,
+    isStudent: false
+  };
+
+  console.log(person["age"]);
+
+  console.log(typeof("hello world"));
